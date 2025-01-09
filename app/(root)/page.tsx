28b85fa7211a086f73c6b7878a1b588fc371bc15
Patrik Zajac <patrik.zajac145@gmail.com>
2025-01-09 16:11:06 +0100

    social auth with github and google

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,22 @@
-const Home = () => {
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+const Home = async () => {
+  const session = await auth();
+
+  console.log(session);
   return (
     <>
-      {/* <h1 className="h1-bold">Home</h1> */}
-      {/* <h1 className="h1-bold font-space-grotesk">Home</h1> */}
+      <form
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+        className="px-10 pt-[100px]"
+      >
+        <Button>Logout</Button>
+      </form>
     </>
   );
 };
