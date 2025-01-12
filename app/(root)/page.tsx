7f5136e75c010f3c046838c86c307bcb0145b3e8
Patrik Zajac <patrik.zajac145@gmail.com>
2025-01-12 21:05:26 +0100

    mobile navigation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,24 +1,10 @@
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
+import { auth } from "@/auth";
 
 const Home = async () => {
   const session = await auth();
 
   console.log(session);
-  return (
-    <>
-      <form
-        action={async () => {
-          "use server";
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-        className="px-10 pt-[100px]"
-      >
-        <Button>Logout</Button>
-      </form>
-    </>
-  );
+  return <></>;
 };
 
 export default Home;
