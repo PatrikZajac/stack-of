432fbcca1cc04f3d88e09b241805bebf43cd3268
Patrik Zajac <patrik.zajac145@gmail.com>
2025-01-23 10:48:02 +0100

    fix account model

--- database/account.model.ts
+++ database/account.model.ts
@@ -1,15 +1,16 @@
-import mongoose, { Schema, Types } from "mongoose";
+import { Schema, model, models, Types, Document } from "mongoose";
 
 export interface IAccount {
   userId: Types.ObjectId;
   name: string;
   image?: string;
   password?: string;
-  provider: string;
+  provider: string; // e.g., 'github', 'google', 'credentials'
   providerAccountId: string;
 }
 
-const AccountSchema = new mongoose.Schema<IAccount>(
+export interface IAccountDoc extends IAccount, Document {}
+const AccountSchema = new Schema<IAccount>(
   {
     userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
     name: { type: String, required: true },
@@ -21,8 +22,6 @@ const AccountSchema = new mongoose.Schema<IAccount>(
   { timestamps: true }
 );
 
-const Account =
-  mongoose.models?.Account ||
-  mongoose.model<IAccount>("Account", AccountSchema);
+const Account = models?.Account || model<IAccount>("Account", AccountSchema);
 
 export default Account;
